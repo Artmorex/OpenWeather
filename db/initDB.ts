ab28c135d00c4a91b1c885cb8d12b7fcde3350e0
artmorex <artmorex@gmail.com>
2024-04-25 23:45:41 +0300

    Update initDB.ts

--- db/initDB.ts
+++ db/initDB.ts
@@ -1,10 +1,19 @@
 export default `
-create table TEST_OW_SB
-(
-    id    text not null
-        constraint TEST_OW_SB_pk
-            primary key,
-    value text
+-- public.WeatherReports definition
+
+-- Drop table
+
+-- DROP TABLE WeatherReports;
+
+CREATE TABLE WeatherReports (
+	report_id uuid NOT NULL,
+	lat float8 NOT NULL,
+	long float8 NOT NULL,
+	timezone varchar NULL,
+	timezone_offset varchar NULL,
+	"current" jsonb NULL,
+	hourly jsonb NULL,
+	daily jsonb NULL
 );
 
 alter table TEST_OW_SB
